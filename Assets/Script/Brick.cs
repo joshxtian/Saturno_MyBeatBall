b6bf73dd90b1a35f18a10494b7dd678a6a436e5d
joshxtian <jaysaturno98@gmail.com>
2022-01-03 13:38:25 +0800

    [code-along-progress] 2nd part

--- Assets/Script/Brick.cs
+++ Assets/Script/Brick.cs
@@ -6,11 +6,17 @@ public class Brick : MonoBehaviour
 {
     public int hits = 1;
     public int points = 10;
+    public Vector3 rotator;
+    public Material hitMaterial;
+    Material orgMaterial;
+    Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        renderer = GetComponent<Renderer>();
+        orgMaterial = renderer.sharedMaterial;
     }
 
     // Update is called once per frame
@@ -19,10 +25,19 @@ public class Brick : MonoBehaviour
         
     }
 
+    
+
     private void OnCollisionEnter(Collision collision) {
         hits--;
         if(hits <= 0) {
             Destroy(gameObject);
         }
+
+        renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterial", 0.05f);
+    }
+
+    void RestoreMaterial() {
+        renderer.sharedMaterial = orgMaterial;
     }
 }
